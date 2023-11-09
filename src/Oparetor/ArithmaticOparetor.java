c2a477349b739a9836748ae9fae5193d232f5983
sadiahera <sadiahera95@gmail.com>
2023-11-09 15:33:10 -0500

    sending code

--- src/Oparetor/ArithmaticOparetor.java
+++ src/Oparetor/ArithmaticOparetor.java
@@ -4,18 +4,18 @@ public class ArithmaticOparetor {
    // +, - , * , /
 
    public void arithmatic(){
-   int a = 15;
+   int a = 14;
    int b = 30;
    int c = a+b;
    int d = c;
    int e = d;
-   System.out.println("15+30 = " + e);
+   System.out.println("14+30 = " + e); // + is called concatenation
    int f = b-a;
-   System.out.println("30-15 = " + f);
-   int g =a+b;
-   System.out.println("15*30 = " + g);
-   int h =b/a;
-   System.out.println("30/15 = " +h);
+   System.out.println("30-14 = " + f);
+   int g =a*b;
+   System.out.println("14*30 = " + g);
+   float h =(float) b/a; // 2.14
+   System.out.println("30/14 = " +h);
 
    }
 
@@ -32,11 +32,11 @@ public class ArithmaticOparetor {
     public static void main(String[] args) {
         ArithmaticOparetor obj = new ArithmaticOparetor(); // instatiated the class /
        // created an object of the class
-        obj.arithmatic();
+       // obj.arithmatic();
         newArithmatic( 15 , 30);// argument
-        System.out.println("--------");
-        newArithmatic( 10, 20);
-        System.out.println("-------");
+       System.out.println("--------");
+       newArithmatic( 10, 20);
+       System.out.println("-------");
         newArithmatic(30 ,60);
 
     }
---
+++ src/Oparetor/LogicalOperators.java
@@ -0,0 +1,4 @@
+package Oparetor;
+
+public class LogicalOperators {
+}
